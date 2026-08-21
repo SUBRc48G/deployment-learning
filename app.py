70913e2c6145434dcaf549c4f691a41f0ab2b438
subrat <subrat033@gmail.com>
2026-08-21 08:18:47 +0000

    Test Jenkins deployment v2

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-print("Hello from my Linux deployment lab!,i am in test-deployement branch")
+from flask import Flask
+import psycopg2
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    conn = psycopg2.connect(
+        host="postgres",
+        database="edumind",
+        user="edumind",
+        password="edumindpass"
+    )
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM students;")
+    students = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return str(students)
+
+
+app.run(host="0.0.0.0", port=5000)
