bfcc4c16fcdf104646396c5b170cd57bf9034f64
subrat <subrat033@gmail.com>
2026-08-22 06:51:08 +0000

    Add automated CI tests

--- tests/test_deployed_app.py
+++ tests/test_deployed_app.py
@@ -8,7 +8,7 @@ def test_health():
     response = requests.get(f"{BASE_URL}/health")
 
     assert response.status_code == 200
-    assert response.text == "OK"
+    assert response.text == "FAIL"
 
 
 def test_home():
